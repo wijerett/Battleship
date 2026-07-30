6bbf20dcaf898d2f560afde8c0183f62af4e2915
wijerett <winnettjeremy@gmail.com>
2026-07-30 05:26:00 -0400

    setting up environment for battleship

---
+++ game.js
@@ -0,0 +1,36 @@
+
+
+//ship class or factory
+
+//hit function inside ship
+
+//isSunk function when ship takes x amount of hits
+
+//gameboard class or factory
+
+//gameboard should be able to place ships at specific coordinates by
+//calling the ship factory or class
+
+//receiveAttack function that takes a pair of coordinates, determines
+//if attack hit a ship then sends hit function to the correct ship or 
+//records coordinates of the missed shot
+
+//keep track of missed shots
+
+//gameboard should be able to keep track of missed shots
+
+//gameboard should be able to report if all ships are sunk or not
+
+//player class or factory
+
+//real player vs computer player
+
+//each player object contain its own gameboard
+
+//import classes/factories into another file, drive the game using event
+//listeners to interact with the objects. create a module that helps 
+//manage action that should happen in the DOM
+
+//Do all of the above before crafting user interface
+
+//
