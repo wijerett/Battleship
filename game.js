ba2627892fa295c814c3f21c87ecaa7797631695
wijerett <winnettjeremy@gmail.com>
2026-07-31 10:04:57 -0400

    creating skeleton of classes and functions

--- game.js
+++ game.js
@@ -2,35 +2,62 @@
 
 //ship class or factory
 
-//hit function inside ship
 
-//isSunk function when ship takes x amount of hits
+export class Ship {
+    constructor() {
+        this.length = length;
+        this.hits = null;
+        this.sunk = null;
+    }
 
-//gameboard class or factory
+    hit() {
+        //if hit is detected this.hits +1
 
-//gameboard should be able to place ships at specific coordinates by
-//calling the ship factory or class
+    }
 
-//receiveAttack function that takes a pair of coordinates, determines
-//if attack hit a ship then sends hit function to the correct ship or 
-//records coordinates of the missed shot
+    isSunk() {
+        //calculates whether a ship is considered sunk based on its length
+        //and the number of hits it has received
+    }
+}
 
-//keep track of missed shots
 
-//gameboard should be able to keep track of missed shots
 
-//gameboard should be able to report if all ships are sunk or not
 
-//player class or factory
 
-//real player vs computer player
 
-//each player object contain its own gameboard
+export class Gameboard {
+
+    //place ships at specific coordinates by calling ship class
+    
+    
+    //receiveAttack function that takes a pair of coordinates, determines
+
+
+    //if attack hit a ship then sends hit function to the correct ship or 
+    //records coordinates of the missed shot
+
+    //gameboard should be able to keep track of missed shots
+
+    //gameboard should be able to report if all ships are sunk or not
+
+}
+
+
+
+export class Player {
+
+    //real player vs computer player
+
+    //each player object contain its own gameboard
+
+}
+
+
+
 
 //import classes/factories into another file, drive the game using event
 //listeners to interact with the objects. create a module that helps 
 //manage action that should happen in the DOM
 
 //Do all of the above before crafting user interface
-
-//
