57e9b8c781ff23110e5d63b18412541b021fd27b
wijerett <winnettjeremy@gmail.com>
2026-08-07 21:49:43 -0400

    tests run

--- game.test.js
+++ game.test.js
@@ -1,19 +1,33 @@
+import { describe } from "yargs";
 import { Ship } from "./game";
+import { beforeEach } from "jest-circus";
+
+
+
+const shipLengths = new Map([
+    ['carrier', 5],
+    ['battleship', 4],
+    ['cruiser', 3],
+    ['submarine', 2],
+    ['patrol', 1]
+]);
+
+const shipHits = new Map([
+    ['carrier', 0],
+    ['battleship', 0],
+    ['cruiser', 0],
+    ['submarine', 0],
+    ['patrol', 0]
+]);
 
 
 
 
 const ship = new Ship();
 
-
+describe('shipsValues', () => {})
 
 test('ship exists', () => {
     expect(ship).toEqual({ "length": undefined, "hits": 0, "sunk": false})
 });
 
-test('populates length and hits', () => {
-    var pop = ship.populateShip();
-    ship.populateShip('carrier', 2);
-    ship.populateShip('battleship', 3);
-    expect(ship.pop).toBe(('carrier', 2), ('battleship', 3))
-});
