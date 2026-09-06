50b6271b0c49e15ce2e375824263d7bfb62d0d65
wijerett <winnettjeremy@gmail.com>
2026-09-06 19:31:52 -0400

    started work on player class

---
+++ src/player.js
@@ -0,0 +1,18 @@
+
+
+import { Ship } from "./game";
+import { Gameboard } from "./board";
+
+export class Player {
+    constructor() {
+
+    }
+
+    realPlayer() {
+        
+    }
+
+    compPlayer() {
+
+    }
+}
