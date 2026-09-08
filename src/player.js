7abad7d223434286806c4f6d336ff8da812a5bf4
wijerett <winnettjeremy@gmail.com>
2026-09-08 09:59:02 -0400

    working on player class

--- src/player.js
+++ src/player.js
@@ -1,20 +1,28 @@
 
 
-import { Ship } from "./game";
-import { Gameboard } from "./board";
+import { Ship } from "./game.js";
+import { Gameboard } from "./board.js";
+
+
 
 export class Player {
     constructor() {
-        this.board = Gameboard(10);
-        this.ship = Ship();
+        this.board = new Gameboard(10);
+        this.ship = new Ship();
     }
 
+
     realPlayer() {
+        
         let playerBoard = this.board;
+        
+        const ship = this.board.placeShips(4, 0, 0, 'horizontal');
+        
         //need to call a new gameboard
         //populate ships and provide coordinates
         //use hit function on board
         //track misses and hits on ships
+        return ship;
     }
 
     compPlayer() {
@@ -22,3 +30,7 @@ export class Player {
         
     }
 }
+
+let player = new Player;
+
+console.log(player.realPlayer());
