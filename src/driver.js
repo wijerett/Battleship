548d26245db34ed69ab41ff707413d57ead0d67e
wijerett <winnettjeremy@gmail.com>
2026-09-22 05:22:09 -0400

    added gameboard arrays to innerHTML of a div figuring out how to arrange them into a gameboard style box

--- src/driver.js
+++ src/driver.js
@@ -5,6 +5,28 @@ import { Player } from "./player.js";
 import { Ship } from "./game.js";
 
 
+const startBtn = document.querySelector('#startBtn');
+
+
+startBtn.addEventListener('click', event => {
+    event.preventDefault();
+    const player = new Player();
+    player.setup();
+    const opponent = new Player();
+    opponent.setup();
+    player.opponentBoard = opponent.board;
+    opponent.opponentBoard = player.board;
+    const board1 = document.createElement("div");
+    board1.innerHTML = player.board.board;
+    const boardBox1 = document.getElementById('board1');
+    boardBox1.appendChild(board1);
+    const board2 = document.createElement("div");
+    board2.innerHTML = opponent.board.board;
+    const boardBox2 = document.getElementById('board2');
+    boardBox2.appendChild(board2);
+    console.log(player.board.board);
+    console.log(opponent.board.board);
+});
 
 
 
@@ -18,5 +40,3 @@ export class driveGame {
         
     }
 }
-
-console.log("hello");
